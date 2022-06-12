35dca336cfc5835caf07cab50d1da2176633b1bb
Tavori2 <tavori.2.pt@gmail.com>
2022-06-12 03:40:20 +0300

    a

--- osh-balance/osh-balance.js
+++ osh-balance/osh-balance.js
@@ -24,13 +24,29 @@ function waitUntilElementLoaded(selector) {
 	});
 }
 
-waitUntilElementLoaded('.osh-balance', 5000).then(function (element) {
+waitUntilElementLoaded('osh-balance', 5000).then(function (element) {
 	// element found and available
 	try {
 		// decrypt the json
 		var decrypted = CryptoJS.AES.decrypt(account, encSecret);
+		let accountJson = JSON.parse(decrypted.toString(CryptoJS.enc.Utf8));
 
-		var oshContainer = document.querySelector('.osh-balance').innerText = decrypted.toString(CryptoJS.enc.Utf8);
+		let populatedTemplate = document.getElementById('osh-balance').innerHTML;
+		var oshContainer = document.querySelector('.osh-summary');
+
+		populatedTemplate = populatedTemplate.replaceAll('{{update-date}}',accountJson.update-date);
+		populatedTemplate = populatedTemplate.replaceAll('{{balance.current}}',accountJson.balance.current);
+		populatedTemplate = populatedTemplate.replaceAll('{{balance.cash}}',accountJson.balance.cash);
+		populatedTemplate = populatedTemplate.replaceAll('{{income.tax}}',accountJson.income.tax);
+		populatedTemplate = populatedTemplate.replaceAll('{{income.other}}',accountJson.income.other);
+		populatedTemplate = populatedTemplate.replaceAll('{{income.total}}',accountJson.income.total);
+		populatedTemplate = populatedTemplate.replaceAll('{{expenses.fixed}}',accountJson.expenses.fixed);
+		populatedTemplate = populatedTemplate.replaceAll('{{expenses.unexpected}}',accountJson.expenses.unexpected);
+		populatedTemplate = populatedTemplate.replaceAll('{{expenses.total}}',accountJson.expenses.total);
+		populatedTemplate = populatedTemplate.replaceAll('{{paiedAppartment}}',accountJson.paiedAppartment);
+		populatedTemplate = populatedTemplate.replaceAll('{{unpaiedAppartmentsNumbers}}',accountJson.unpaiedAppartmentsNumbers);
+
+		oshContainer.innerText = populatedTemplate;
 	}
 	catch (err) {
 		// error occurred
