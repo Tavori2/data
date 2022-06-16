0d46877579262876d37bd9e61a5d1015eb3d55c1
Tavori2 <tavori.2.pt@gmail.com>
2022-06-16 07:00:13 +0300

    updated

---
+++ osh-balance/account_decrypted.js
@@ -0,0 +1,28 @@
+var account2 = `﻿{
+    "updateDate": "08/06/2022",
+    "balance": {
+        "openning": "12,513",
+        "current": "27,306",
+        "cash": "3,670"
+    },
+    "account": {
+        "bank": "52",
+        "bankName": "(פאגי - בנק פועלי אגודת ישראל)",
+        "branch": "181",
+        "accountNum": "199082",
+        "branchAddress": "מרכז קרסו, ישעיהו משורר 11 פ\"ת 4931946"
+    },
+    "income": {
+        "tax": "80,635",
+        "other": "5,560",
+        "total": "86,195"
+    },
+    "expenses": {
+        "fixed": "-52,344",
+        "unexpected": "-8,775",
+        "total": "-61,119"
+    },
+    "paiedAppartment": "17",
+    "unpaiedAppartmentsNumbers": "2, 4, 5, 6, 7, 8, 11, 14, 15, 17, 19, 20, 21, 22, 23, 25, 27, 29, 32, 33, 37, 38, 39"
+}
+`;
