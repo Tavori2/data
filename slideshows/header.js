f3c65a2f21c3bfedb2b77274569031fd8c8651c1
Tavori2 <tavori.2.pt@gmail.com>
2022-06-17 21:48:26 +0300

    z

--- slideshows/header.js
+++ slideshows/header.js
@@ -1,79 +1,69 @@
 function waitUntilElementLoaded(selector) {
-	var timeout = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 0;
+    var timeout = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 0;
 
-	var start = performance.now();
-	var now = 0;
+    var start = performance.now();
+    var now = 0;
 
-	return new Promise(function (resolve, reject) {
-		var interval = setInterval(function () {
-			var element = document.querySelector(selector);
+    return new Promise(function (resolve, reject) {
+        var interval = setInterval(function () {
+            var element = document.querySelector(selector);
 
-			if (element instanceof Element) {
-				clearInterval(interval);
+            if (element instanceof Element) {
+                clearInterval(interval);
 
-				resolve();
-			}
+                resolve();
+            }
 
-			now = performance.now();
+            now = performance.now();
 
-			if (now - start >= timeout) {
-				reject("Could not find the element " + selector + " within " + timeout + " ms");
-			}
-		}, 100);
-	});
+            if (now - start >= timeout) {
+                reject("Could not find the element " + selector + " within " + timeout + " ms");
+            }
+        }, 100);
+    });
+}
+
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+// show specific slide
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
 }
 
 waitUntilElementLoaded('.slideshow-container', 5000).then(function (element) {
-	// element found and available
-	try {
+    // element found and available
+    try {
 
         let slideIndex = 1;
         showSlides(slideIndex);
-        
-        // Next/previous controls
-        function plusSlides(n) {
-          showSlides(slideIndex += n);
-        }
-        
-        // Thumbnail image controls
-        function currentSlide(n) {
-          showSlides(slideIndex = n);
-        }
-        
-        function showSlides(n) {
-          let i;
-          console.log("1");
-          let slides = document.getElementsByClassName("mySlides");
-          console.log("2");
-          let dots = document.getElementsByClassName("dot");
-          console.log("3");
-          if (n > slides.length) {slideIndex = 1}
-          console.log("4");
-          if (n < 1) {slideIndex = slides.length}
-          console.log("5");
-          for (i = 0; i < slides.length; i++) {
-            console.log("5." + i);
-            slides[i].style.display = "none";
-          }
-          console.log("7");
-          for (i = 0; i < dots.length; i++) {
-            console.log("7." + i);
-            dots[i].className = dots[i].className.replace(" active", "");
-          }
-          console.log("8");
-          slides[slideIndex-1].style.display = "block";
-          console.log("9");
-          dots[slideIndex-1].className += " active";
-          console.log("10");
-        }
-	}
-	catch (err) {
-		// error occurred
-		console.log("Failed to show slideshow-container due to: " + err);
-	}
+    }
+    catch (err) {
+        // error occurred
+        console.log("Failed to show slideshow-container due to: " + err);
+    }
 }).catch(function () {
-	// element not found within 5000 milliseconds
-	console.log("Failed to find .slideshow-container for 5 sec");
+    // element not found within 5000 milliseconds
+    console.log("Failed to find .slideshow-container for 5 sec");
 });
 
 
