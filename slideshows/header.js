bac19d19d1db4e0172106b73cc38994ff745f099
Tavori2 <tavori.2.pt@gmail.com>
2022-06-17 22:48:00 +0300

    r

--- slideshows/header.js
+++ slideshows/header.js
@@ -23,35 +23,25 @@ function waitUntilElementLoaded(selector) {
     });
 }
 
-let slideIndex = 1;
-
-// Next/previous controls
-function plusSlides(n) {
-    showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
+let slideIndex = 0;
 
 // show specific slide
-function showSlides(n) {
+function showSlides() {
     let i;
     let slides = document.getElementsByClassName("mySlides");
-    if (n > slides.length) { slideIndex = 1 }
-    if (n < 1) { slideIndex = slides.length }
     for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
+      slides[i].style.display = "none";
     }
-
-    slides[slideIndex - 1].style.display = "block";
-}
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}
+    slides[slideIndex-1].style.display = "block";
+    setTimeout(showSlides, 2000); // Change image every 2 seconds
+  }
 
 waitUntilElementLoaded('.slideshow-container', 5000).then(function (element) {
     // element found and available
     try {
-        showSlides(slideIndex);
+        showSlides();
     }
     catch (err) {
         // error occurred
