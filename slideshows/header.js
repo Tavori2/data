53fa9d744d7b55b6ee7423d69ec5ba49b589842c
Tavori2 <tavori.2.pt@gmail.com>
2022-06-17 21:21:47 +0300

    s

--- slideshows/header.js
+++ slideshows/header.js
@@ -13,16 +13,27 @@ function currentSlide(n) {
 
 function showSlides(n) {
   let i;
+  console.log("1");
   let slides = document.getElementsByClassName("mySlides");
+  console.log("2");
   let dots = document.getElementsByClassName("dot");
+  console.log("3");
   if (n > slides.length) {slideIndex = 1}
+  console.log("4");
   if (n < 1) {slideIndex = slides.length}
+  console.log("5");
   for (i = 0; i < slides.length; i++) {
+    console.log("5." + i);
     slides[i].style.display = "none";
   }
+  console.log("7");
   for (i = 0; i < dots.length; i++) {
+    console.log("7." + i);
     dots[i].className = dots[i].className.replace(" active", "");
   }
+  console.log("8");
   slides[slideIndex-1].style.display = "block";
+  console.log("9");
   dots[slideIndex-1].className += " active";
+  console.log("10");
 } 
