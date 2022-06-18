f22958046d0563c99b0dcaee73c8b1c8d14d2a5e
Tavori2 <tavori.2.pt@gmail.com>
2022-06-18 03:12:48 +0300

    m

--- account/summary.js
+++ account/summary.js
@@ -1,29 +1,3 @@
-
-function waitUntilElementLoaded(selector) {
-	var timeout = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 0;
-
-	var start = performance.now();
-	var now = 0;
-
-	return new Promise(function (resolve, reject) {
-		var interval = setInterval(function () {
-			var element = document.querySelector(selector);
-
-			if (element instanceof Element) {
-				clearInterval(interval);
-
-				resolve();
-			}
-
-			now = performance.now();
-
-			if (now - start >= timeout) {
-				reject("Could not find the element " + selector + " within " + timeout + " ms");
-			}
-		}, 100);
-	});
-}
-
 waitUntilElementLoaded('#balance', 5000).then(function (element) {
 	// element found and available
 	try {
--- contacts/contacts.js
+++ contacts/contacts.js
@@ -1,28 +1,3 @@
-function waitUntilElementLoaded(selector) {
-	var timeout = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 0;
-
-	var start = performance.now();
-	var now = 0;
-
-	return new Promise(function (resolve, reject) {
-		var interval = setInterval(function () {
-			var element = document.querySelector(selector);
-
-			if (element instanceof Element) {
-				clearInterval(interval);
-
-				resolve();
-			}
-
-			now = performance.now();
-
-			if (now - start >= timeout) {
-				reject("Could not find the element " + selector + " within " + timeout + " ms");
-			}
-		}, 100);
-	});
-}
-
 function longPressed(evt, tenantId) {
 	let currentCard = document.querySelector('.menu-' + tenantId);
 	let fullScreen = document.querySelector('.fullScreen');
